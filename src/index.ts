15f7cef8ca74a55605760844701b5a6aa7668cf9
weaselau <weasel@outlook.com.au>
2022-10-17 02:58:00 +1000

    Restored Previous Code

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ async function ProjectZomboid() {
             _message.edit({
                 embeds: [
                     new EmbedBuilder()
-                        .setTitle('Project Zomboid...')
+                        .setTitle('Project Zomboid')
                         .setColor(resolveColor('#db2525'))
                         .setDescription('>>> The Server is Offline...')
                         .setTimestamp(new Date())
@@ -66,5 +66,3 @@ async function ProjectZomboid() {
 
 
 }
-
-console.log('Hello World')
