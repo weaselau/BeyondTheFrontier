995c3c461cb3a57ebd474076f8ce7df67ce983a4
Koda <koda@horizons.gg>
2022-11-06 22:37:44 +1000

    Update commands.ts

--- src/lib/discord/commands.ts
+++ src/lib/discord/commands.ts
@@ -4,6 +4,15 @@ import { SlashCommandBuilder } from "discord.js"
 
 
 
+//? Variables
+
+const RoleChoices = [
+    { name: 'Project Zomboid', value: 'project_zomboid' },
+    { name: 'Minecraft', value: 'minecraft' }
+]
+
+
+
 //? Commands
 
 const Commands = [
@@ -35,8 +44,35 @@ const Commands = [
 
     new SlashCommandBuilder()
         .setName('connection')
-        .setDescription('Connection Tab')
+        .setDescription('Connection Tab'),
 
+
+        
+    new SlashCommandBuilder()
+        .setName('roles')
+        .setDescription('Self Assignable Roles')
+
+        .addSubcommand(subcommand => subcommand
+            .setName('add')
+            .setDescription('Add a Role to yourself')
+            .addStringOption(option => option
+                .setName('role')
+                .setDescription('Role to be added')
+                .setRequired(true)
+                .setChoices(...RoleChoices)
+            )
+        )
+
+        .addSubcommand(subcommand => subcommand
+            .setName('remove')
+            .setDescription('Remove a Role from yourself')
+            .addStringOption(option => option
+                .setName('role')
+                .setDescription('Role to be removed')
+                .setRequired(true)
+                .setChoices(...RoleChoices)
+            )
+        )
 
 
 ]
