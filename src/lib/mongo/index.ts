5c02e2e9be440ff2ac7cd1ab4bd8e38bf792adf5
Index <callummonks21@gmail.com>
2022-11-07 23:04:39 +1000

    dataaaaaa

---
+++ src/lib/mongo/index.ts
@@ -0,0 +1,20 @@
+import * as Mongo from 'mongodb'
+import * as Config from '@lib/config'
+
+export const Collections: {
+    Tickets?: Mongo.Collection
+} = {}
+
+export async function connect() {
+    const client: Mongo.MongoClient = new Mongo.MongoClient(Config.default.discord.mongourl)
+    await client.connect()
+
+    const db: Mongo.Db = client.db('BTF')
+
+    const Tickets: Mongo.Collection = db.collection('tickets')
+
+    Collections.Tickets = Tickets
+
+
+    console.log(`Successfully connected to database: ${db.databaseName}`)
+}
