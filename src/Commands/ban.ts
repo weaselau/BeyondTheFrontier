f736eb70ffd5213ea4e57009f6065b2840f2a1ba
weaselau <weasel@outlook.com.au>
2022-11-08 15:17:36 +1000

    Update ban.ts

--- src/Commands/ban.ts
+++ src/Commands/ban.ts
@@ -1,17 +1,19 @@
 //? Dependencies
 
 
+
 import { _client } from '../lib/discord/index'
 import { Client, ChatInputCommandInteraction, CacheType, EmbedBuilder, resolveColor, Message } from "discord.js"
 import Config from '@lib/config'
+
 
 
 //? vars
 
 
 
-const punishments = '1039406411718873109' //? punishments channel
-//? or send to data base
+
+//? Ban and kicks needed to send to data base 
 
 
 //? Command
