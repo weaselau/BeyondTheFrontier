b87e0477bf033c527ed20b97f402cf64310d85fa
weaselau <weasel@outlook.com.au>
2022-11-08 15:16:26 +1000

    Update ban.ts

--- src/Commands/ban.ts
+++ src/Commands/ban.ts
@@ -1,15 +1,25 @@
 //? Dependencies
 
 
+import { _client } from '../lib/discord/index'
+import { Client, ChatInputCommandInteraction, CacheType, EmbedBuilder, resolveColor, Message } from "discord.js"
+import Config from '@lib/config'
 
-import { ChatInputCommandInteraction, CacheType, EmbedBuilder, resolveColor } from "discord.js"
 
+//? vars
+
+
+
+const punishments = '1039406411718873109' //? punishments channel
+//? or send to data base
 
 
 //? Command
 
 
 
-export default {
-    
+export default (interaction: ChatInputCommandInteraction) => {
+
+
+
 }
