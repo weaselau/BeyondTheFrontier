2f3d30441b6e43f650621a5e2f27cc6ede02ce36
Index <callummonks21@gmail.com>
2022-12-05 06:38:27 +1000

    Finally Got Tickets To Work

--- src/lib/discord/events.ts
+++ src/lib/discord/events.ts
@@ -117,30 +117,102 @@ export function InteractionSelectMenu(interaction: Discord.SelectMenuInteraction
     }
 }
 
-export function ButtonPress(interaction: Discord.ButtonInteraction) {
+export async function ButtonPress(interaction: Discord.ButtonInteraction) {
 
-    //TODO: Add Interaction ID Switch Statement
+    switch (interaction.customId) {
+        case 'Primary':
+            let OpenTicketsChannel = _client.channels.cache.get('1045376153336172564') //Opens Tickets Channel ID
+            let Guild: Discord.Guild | undefined = _client.guilds.cache.get('614680459728650250')
 
-    let OpenTicketsChannel = _client.channels.cache.get('1045376153336172564') //Opens Tickets Channel ID
-    let Guild: Discord.Guild | undefined = _client.guilds.cache.get('614680459728650250')
+            if(OpenTicketsChannel === null || OpenTicketsChannel === undefined) return;
+            OpenTicketsChannel.type !== Discord.ChannelType.GuildCategory? { return: null }: null; // I Dont Know What This Is
+            if(!Guild) return;
+            if(!Collections.Tickets) return console.error('Unable To Find The Tickets Collection')
+            if(!interaction.guild) return //? This is required for some reason
 
-    if(OpenTicketsChannel === null || OpenTicketsChannel === undefined) return;
-    OpenTicketsChannel.type !== Discord.ChannelType.GuildCategory? { return: null }: null; // I Dont Know What This Is
-    if(!Guild) return;
-    if(!Collections.Tickets) return console.error('Unable To Find The Tickets Collection')
+            let TicketMongo = Collections.Tickets.insertOne({})
+            let amountOfDocs = await Collections.Tickets.countDocuments()
 
-    console.log('Button Detected')
+            let newChannel = await Guild.channels.create({name: `${interaction.user.username.toLowerCase()}-${amountOfDocs + 1}`, parent: OpenTicketsChannel, permissionOverwrites: [
+                { id: interaction.user.id, allow: [Discord.PermissionFlagsBits.ViewChannel] },
+                { id: interaction.guild.roles.everyone, deny: [Discord.PermissionFlagsBits.ViewChannel] },
+                { id: '1049056577346343053', allow: [Discord.PermissionFlagsBits.ViewChannel]}
+            ]})
+            if(!newChannel.isTextBased()) return;
+            newChannel.send({embeds: [
+                new Discord.EmbedBuilder()
+                .setColor(Discord.resolveColor('#567fe8'))
+                .setTitle('Ticket Controls')
+            ], components: [
+                new Discord.ActionRowBuilder<Discord.ButtonBuilder>()
+                .addComponents([
+                    new Discord.ButtonBuilder()
+                    .setCustomId('button-save')
+                    .setStyle(Discord.ButtonStyle.Secondary)
+                    .setLabel('Save Ticket'),
+                    new Discord.ButtonBuilder()
+                    .setCustomId('button-close')
+                    .setStyle(Discord.ButtonStyle.Danger)
+                    .setLabel('Close Ticket')
+                ])
+            ]})
 
-    /*
-    Collections.Tickets.insertOne({})
+            newChannel.send('<@' + interaction.user.id + '> <@&1049056577346343053>')
 
-    Guild?.channels.create({name: `${interaction.user.username.toLowerCase()}-${Collections.Tickets.countDocuments()}`, parent: OpenTicketsChannel})
+            Collections.Tickets.updateOne(TicketMongo, {$set: { channelID: newChannel.id, author: interaction.user.id }})
 
-    */
+            interaction.deferUpdate()
+        break;
+    
+        case 'button-close':
+            if(interaction.channel?.type !== Discord.ChannelType.GuildText || interaction.channel.parent?.id === '1045376256524435497') return
+            interaction.reply({content: 'Are you sure you want to close this ticket? (Close this message to cancel)', ephemeral: true, components: [
+                new Discord.ActionRowBuilder<Discord.ButtonBuilder>()
+                .addComponents([
+                    new Discord.ButtonBuilder()
+                    .setCustomId('button-full-close')
+                    .setStyle(Discord.ButtonStyle.Danger)
+                    .setLabel('Yes, Im Sure')
+                ])
+            ]})
+        break;
 
-    //? I Give Up, I've Added Checks Im Not Wasting More Time On This Lets Just Get Koda.
-    //? I DO Know The Code Is Fucked, Yes. But I'm Trying To Remove Errors That Typescript Keeps Giving Me Not Actually "Debug" Persay.
-    //? I Also Know This Code WON'T Work But That Wasnt My Goal, My Goal Was To Remove Any Errors Arising From Any Types Not Working Even
-    //? Though IVE ADDED FUCKING CHECKS
-    //? I'm Tired. I'm Cold. I'm Going To Sleep. Fuck. This. Language.
+        case 'button-save':
+            Collections.Tickets?.find().forEach(doc => {
+                let messageArray: Array<any> = [  ]
+                doc.channelID !== interaction.channel?.id? { return: null }: null
+                interaction.channel?.messages.cache.forEach(msg => { //? Due to a flaw in discord API or DJS i cannot retrieve messages from when the bot isnt running (shouldnt be an issue on a server)
+                    messageArray.push({
+                        author: msg.author.id,
+                        timestamp: msg.createdTimestamp,
+                        content: msg.content,
+                        edited: msg.editedAt === null? false: true
+                    })
+                })
+                Collections.Tickets?.updateOne(doc, {$set: { messages: messageArray }})
+                interaction.deferUpdate()
+            })
+        break;
+
+        case 'button-full-close':
+            let ClosedTicketsChannel = _client.channels.cache.get('1045376256524435497')
+            if(interaction.channel?.type !== Discord.ChannelType.GuildText) return;
+            interaction.channel.edit({parent: ClosedTicketsChannel})
+            interaction.deferUpdate()
+            interaction.deleteReply()
+            interaction.channel.send('Ticket has been closed by <@' + interaction.user + '>')
+        break;
+    }
+
+    
+
 }
+
+export function onChannelDelete(ch: Discord.Channel) { 
+    //? Used to remove deleted channels from the database
+    Collections.Tickets?.find().forEach(doc => {
+        doc.channelID === ch.id? Collections.Tickets?.deleteOne(doc): null
+    })
+}
+
+//? Javascript > Typescript
