a4f9bf311b788bfea69bdf2bb059213887e6e2f6
weaselau <weasel@outlook.com.au>
2023-01-12 13:07:10 +1000

    Update events.ts

--- src/lib/discord/events.ts
+++ src/lib/discord/events.ts
@@ -111,6 +111,7 @@ export function InteractionSelectMenu(interaction: Discord.SelectMenuInteraction
             let rwrole = guild.roles.cache.find(role => role.name === 'RimWorld'); if(!rwrole) return;
             if (interaction.values.includes('rw')) Member?.roles.add(rwrole)
 
+            
             let dzrole = guild.roles.cache.find(role => role.name === 'Dayz'); if(!rwrole) return;
             if (interaction.values.includes('dz')) Member?.roles.add(rwrole)
 
