7c7ab35123c26469c7b086c61ba6bb6f24e269da
weaselau <weasel@outlook.com.au>
2023-01-29 12:13:45 +1000

    Update index.ts

--- src/lib/Gamedig/index.ts
+++ src/lib/Gamedig/index.ts
@@ -1,3 +1,3 @@
 export  { default as Minecraft } from './minecraft'
-export  { default as SpaceEngineers } from './spaceengineers'
-export  { default as ProjectZomboid } from './projectzomboid'
+export  { default as SpaceEngineers } from './SpaceEngineers'
+export  { default as ProjectZomboid } from './ProjectZomboid'
