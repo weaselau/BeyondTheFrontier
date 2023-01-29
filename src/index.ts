b93ef83453c88a539c04b70101ecfe566197eb71
weaselau <weasel@outlook.com.au>
2023-01-29 23:25:10 +1000

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 //? Dependencies
 
 import Config from '@lib/config'
-import { Message, EmbedBuilder, resolveColor, ActionRowBuilder, ButtonBuilder, ButtonStyle, SelectMenuBuilder, InteractionCollector, ChatInputCommandInteraction, CacheType, AnyComponentBuilder, ActivityType } from 'discord.js'
 import Client, { Channel } from "@lib/discord"
-import Query from "@lib/gamedig"
 import * as Mongo from 'lib/mongo'
 import { _client } from '@lib/discord/index'
 import Discord from 'discord.js'
@@ -20,6 +18,11 @@ import { Minecraft, ProjectZomboid, SpaceEngineers } from '@lib/Gamedig/index'
 
 import { RulesMenu, InfomationMenu } from '@lib/templates/RulesInfo/index'
 import { RolesMenu } from '@lib/templates/Roles/index'
+
+//? Support
+
+import { SupportChannel } from '@lib/templates/Embeds'
+
 
 
 
@@ -43,39 +46,18 @@ Client()
         RulesMenu()
         InfomationMenu()
         MemberCount()
-
-        //?
-        //? 12 Hour update time
-        //?
-
         setInterval(() => {
             RulesMenu()
             InfomationMenu()
             RolesMenu()
         }, 1000 * 60 * 720)
-
-
-        //?
-        //? 60 minute update time
-        //?
-
         setInterval(() => {
             SupportChannel()
         }, 1000 * 60 * 60)
-
-
-        //?
-        //? 3 minute update time
-        //?
-
         setInterval(() => {
             ProjectZomboid()
             Minecraft()
             SpaceEngineers()
-
-
-
-
             MemberCount()
         }, 1000 * 60 * 3)
     })
@@ -90,71 +72,3 @@ Client()
             }]
         })
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//?
-//? Support Channel
-//?
-
-
-
-async function SupportChannel() {
-
-    const _channel = await Channel(Config.discord.guild, '1039148738628825098')
-    const _message: Message = _channel.messages.cache.get('1039148951305199676') || await _channel.messages.fetch('1039148951305199676')
-
-    if (!_message) return console.log('Support Channel Message could not be found!')
-
-
-    //? Button Menu
-
-
-    const button = new ActionRowBuilder<ButtonBuilder>()
-        .addComponents([
-            new ButtonBuilder()
-                .setCustomId('Primary')
-                .setLabel('Support Ticket')
-                .setEmoji('🎫')
-                .setStyle(ButtonStyle.Success)
-        ])
-
-
-    //? Support Embed 
-
-    _message.edit({
-        embeds: [
-            new EmbedBuilder()
-                .setTitle('**Server Support Channel**')
-                .setColor(resolveColor('#567fe8'))
-                .setDescription('>>> Need Help? Our staff are here to support you and the problems you are facing. \n\n')
-                .setTimestamp(new Date())
-        ], components: [button]
-
-    })
-        .catch(() => {
-            _message.edit({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle('Support Channel')
-                        .setColor(resolveColor('#db2525'))
-                        .setDescription('>>> The Support Channel is Offline...')
-                        .setTimestamp(new Date())
-                ]
-            })
-        })
-}
---
+++ src/lib/templates/Embeds/index.ts
@@ -0,0 +1,1 @@
+export  { default as SupportChannel} from './support'
